1a6e0bb9350ea0f7d47a6a81416d1a7a1c38962c
defendnet@163.com <defendnet@163.com>
2016-11-15 11:35:09 +0800

    add configuration cmd

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(name=name,
 
       packages=['websockify'],
       include_package_data=True,
-      install_requires=['numpy'],
+      install_requires=['numpy','stomp'],
       zip_safe=False,
       entry_points={
         'console_scripts': [
